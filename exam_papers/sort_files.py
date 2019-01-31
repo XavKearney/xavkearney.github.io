2a46d9d7ecb668c2fe50b0d94f9231503982d4c4
Xav Kearney <xavkearney@gmail.com>
2019-01-31 12:44:30 +0000

    Add 2018 papers to exam_papers and improve sorting script

--- exam_papers/sort_files.py
+++ exam_papers/sort_files.py
@@ -2,8 +2,10 @@ import os
 import glob
 from shutil import copyfile
 
+YEAR = "2018"
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
-all_files = glob.glob(os.path.join(dir_path, "2017", "*"))
+all_files = glob.glob(os.path.join(dir_path, YEAR, "*"))
 module_names = {
     "EE1": [os.path.basename(x[0]) for x in os.walk(os.path.join(dir_path, "EE1"))],
     "EE2": [os.path.basename(x[0]) for x in os.walk(os.path.join(dir_path, "EE2"))],
@@ -13,7 +15,7 @@ module_names = {
 for file_path in all_files:
     file_name = os.path.basename(file_path)
     year = file_name[0:3]
-    module_num = file_name[4:7].strip()
+    module_num = file_name[4:7].strip().replace("_", "")
     module_name = None
     for name in module_names[year]:
         if name.startswith(module_num):
@@ -22,12 +24,14 @@ for file_path in all_files:
         print("Could not find: {}-{}".format(year, module_num))
         continue
 
+    _, extension = os.path.splitext(file_name.lower())
+
     if "solutions" in file_name.lower():
-        target_filename = "2017_sols.pdf"
+        target_filename = f"{YEAR}_sols.pdf"
     elif "annotate" in file_name.lower():
-        target_filename = "2017_ann.pdf"
+        target_filename = f"{YEAR}_ann.pdf"
     else:
-        target_filename = "2017.pdf"
+        target_filename = f"{YEAR}.pdf"
     target_path = os.path.join(dir_path, year, module_name, target_filename)
     if os.path.isfile(target_path):
         print("Already exists: {}".format(target_path))
